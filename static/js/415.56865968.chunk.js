afef26f340e38c18ea8b90a10c4d110fa2c89945
Alyonka556 <142902721+Alyonka556@users.noreply.github.com>
2023-12-28 15:49:13 +0000

    Deploying to gh-pages from @ Alyonka556/goit-react-hw-08-phonebook@49aa77ddbf42a01a1454d4a7edd2b6614cf8de78 🚀

---
+++ static/js/415.56865968.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,r,t){t.r(r);var a,n=t(168),c=(t(2791),t(2978)),u=t(184);r.default=function(){return(0,u.jsx)(h,{children:"Homepage"})};var h=c.ZP.h1(a||(a=(0,n.Z)(["\n  margin: 30px 0;\n"])))}}]);
+//# sourceMappingURL=415.56865968.chunk.js.map
